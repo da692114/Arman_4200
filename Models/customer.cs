abdf47d569b444269ec8ed216eeef3ea1384b4ad
da692114 <da692114@COB-CLAB78>
2019-09-11 11:39:40 -0400

    initial model work

---
+++ Models/customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Arman_4200.Models
+{
+    public class customer
+    {
+        //prop tab TWICE
+        public int customerID { get; set; }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public ICollection<order> orders { get; set; }
+
+
+    }
+}
---
+++ Models/order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Arman_4200.Models
+{
+    public class order
+    {
+        public int orderID { get; set; }
+        public int customerID { get; set; }
+        public virtual customer customer { get; set; }
+        public DateTime orderDate { get; set; }
+
+
+    }
+}
